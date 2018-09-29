ad756fb60633ee360aeb2e23bcacd72700b5471b
Michael Pieschel <sixtyhectares@yahoo.com>
2018-09-29 20:58:24 +0000

    automatically backed up by learn

--- db/migrate/01_create_students.rb
+++ db/migrate/01_create_students.rb
@@ -1,6 +1,10 @@
 class CreateStudents < ActiveRecord::Migration[5.1]
   
   def change
-    create_table
+    def change
+    create_table :artists do |t|
+      t.string :name
+    end
+  end
   end
 end
